4d8a0c4ab1c2184c625e5cd866c92d4bce9f2025
Arpit Jaiswal <jaiswalarpit67@gmail.com>
2026-09-16 00:45:24 +0530

    Time: 0 ms (100%), Space: 52.2 MB (83.26%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int x = 0;
+        for (int i = 0; i < nums.size(); i++){
+            if (x < i)
+                return false;
+            else
+                x = max(x,i+nums[i]);
+        }
+        return true;
+    }
+};
